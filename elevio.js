3046e554d162bc77eb56e48f48de9746fb3f07d8
SittipongHAUS <sittiponghaus@H-AMPOS.local>
2022-06-05 16:19:03 +0700

    add tagmngr

--- elevio.js
+++ elevio.js
@@ -1,6 +1,7 @@
 (function() {
   console.log('document loaded..')
   Segment()
+  TagIframe()
 })()
 
 function Segment () {
@@ -9,3 +10,20 @@ function Segment () {
   analytics.page();
   }
 }
+
+function TagIframe () {
+  var ifrm = document.createElement('iframe');
+  ifrm.setAttribute('id', 'tagmanager');
+  ifrm.setAttribute('src', 'https://www.googletagmanager.com/ns.html?id=GTM-NMJKBKD');
+  ifrm.setAttribute('width', '0');
+  ifrm.setAttribute('height', '0');
+  ifrm.setAttribute('style', 'display:none;visibility:hidden')
+  document.body.appendChild(ifrm)
+}
+//Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NMJKBKD');
+
