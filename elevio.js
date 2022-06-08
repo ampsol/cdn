9ff0caa8cae52b77e1b24fc487c1d9018daf6924
Sittipong HAUS <h@sittipong.com>
2022-06-08 14:09:52 +0700

    add Appcues

--- elevio.js
+++ elevio.js
@@ -1,23 +1,35 @@
 const includeHotjar = () => {
-  console.log('document loaded..')
+  console.log('Hotjar Loaded..')
   var script = document.createElement('script');
   script.src = 'https://ampsol.github.io/cdn/hotjar.js';
   script.async = true;
   document.getElementsByTagName('head')[0].appendChild(script);
 }
 const includeSegment = () => {
-  console.log('document loaded..')
+  console.log('Segment Loaded..')
   var script = document.createElement('script');
   script.src = 'https://ampsol.github.io/cdn/segment.js';
   script.async = true;
   document.getElementsByTagName('head')[0].appendChild(script);
 }
 const includeHeap = () => {
-  console.log('document loaded..')
+  console.log('Heap Loaded..')
   var script = document.createElement('script');
   script.src = 'https://ampsol.github.io/cdn/heap.js';
   script.async = true;
   document.getElementsByTagName('head')[0].appendChild(script);
+}
+const includeAppcues = () => {
+  console.log('Appcues Loaded..')
+  var script = document.createElement('script');
+  script.src = 'https://fast.appcues.com/115517.js';
+  script.async = true;
+  document.getElementsByTagName('head')[0].appendChild(script);
+  setTimeout(() => {
+    window.AppcuesSettings = {
+      enableURLDetection: true
+    };
+  }, 1000)
 }
 (function() {
   includeHotjar()
