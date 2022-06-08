849b9a0a2dbe97ec91be8ce5fea39f6f0505d724
Sittipong HAUS <h@sittipong.com>
2022-06-08 09:57:57 +0700

    add heap

--- elevio.js
+++ elevio.js
@@ -12,6 +12,13 @@ const includeSegment = () => {
   script.async = true;
   document.getElementsByTagName('head')[0].appendChild(script);
 }
+const includeHeap = () => {
+  console.log('document loaded..')
+  var script = document.createElement('script');
+  script.src = 'https://ampsol.github.io/cdn/heap.js';
+  script.async = true;
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
 (function() {
   includeHotjar()
   includeSegment()
