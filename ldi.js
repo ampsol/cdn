fc34750db6424c9383e272bea3c4ca379d376749
Sittipong HAUS <h@sittipong.com>
2022-07-05 02:14:35 +0700

    add headway && fix pendo

--- ldi.js
+++ ldi.js
@@ -1,12 +1,10 @@
-const includePendo = () => {
+const includePendo = (apiKey) => {
+  (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
+  v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
+      o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+      y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
+      z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
   console.log('Pendo Loaded..')
-  (function(apiKey){
-    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
-    v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
-        o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
-        y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
-        z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-  })('9f96e989-c030-46d9-7dcb-5ddd97190f5a')
 }
 const includeLOU = () => {
   console.log('Heap Loaded..')
@@ -15,9 +13,8 @@ const includeLOU = () => {
   script.async = true;
   document.getElementsByTagName('head')[0].appendChild(script);
 }
-
 (function() {
-  includePendo()
+  includePendo('9f96e989-c030-46d9-7dcb-5ddd97190f5a')
   includeLOU()
 })()
 
