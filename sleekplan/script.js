d29a8d38ba47d754d619bc9e350e3340d9f85fd6
Sittipong HAUS <h@sittipong.com>
2022-07-25 23:28:46 +0700

    update ux

--- sleekplan/script.js
+++ sleekplan/script.js
@@ -5,3 +5,8 @@ const dismissed = () => {
 	document.getElementById('sleek').classList.add('dismissed')
 	document.getElementById('suggest').classList.add('full')
 }
+
+const hideSuggest = () => {
+	dismissed()
+	document.getElementById('suggest').classList.add('hide')
+}
