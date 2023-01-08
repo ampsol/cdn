b248ac04e40e0f5d1d0c869cfd667b59e7189ab1
Sittipong HAUS <bvmsk.sittipong@gmail.com>
2023-01-09 02:32:08 +0700

    change method to create style tag instead

--- atlas-overrided.js
+++ atlas-overrided.js
@@ -1,5 +1,7 @@
 const hideHS = () => {
-  document.querySelector('hs-cmsf-branding').remove()
+  const styleTag = document.createElement('style')
+  styleTag.innerHTML = 'hs-cmsf-branding { display: none!important; visibility: hidden!important; opacity: 0!important; }'
+  document.querySelector('body').appendChild(styleTag)
 }
 const delayHideHS = () => {
   window.addEventListener('load', hideHS)
